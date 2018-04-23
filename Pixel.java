5f33756c5ead0a24ca592716ab90a3cbee917a00
StaffOfHades <mau.graci@gmail.com>
2018-04-23 16:03:03 -0500

    Transferencia de codigo Python a Java finalizada

--- Pixel.java
+++ Pixel.java
@@ -7,8 +7,10 @@ import javax.imageio.*;
 import javax.swing.*;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.function.UnaryOperator;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 import java.lang.Math;
@@ -18,32 +20,17 @@ class Pixel {
 
    private static final String PATH = 
          "C:\\Users\\Mauricio\\Documents\\UDLAP\\Primavera 2018\\Procesamiento Multimedial\\";
-
-   BufferedImage image;
-   int width;
-   int height;
    
    Pixel() {
-      BufferedImage image = loadImage("face.png");
-      int[] data = getData(image);
-      int[] gray = grayscaleTransformation(data);
-      long timeA = 0, timeB = 0;
-
-      int loops = 10;
-      for(int i = 0; i < loops; i++) {
-         long timeIn = System.currentTimeMillis();
-         inverseOperatorA(gray);
-         long timeOut = System.currentTimeMillis();
-         timeA += timeOut - timeIn;
-         timeIn = System.currentTimeMillis();
-         inverseOperatorB(gray);
-         timeOut = System.currentTimeMillis();
-         timeB += timeOut - timeIn;
-      }
-      
-      System.out.println("TimeA: " + (timeA / loops) + ", TimeB: " + (timeB / loops) );
-      //setData(image, inverse);
-      //showImage(image);
+      BufferedImage image1 = loadImage("bg1.jpg");
+      BufferedImage image2 = loadImage("bg2.jpg");
+      int[] data1 = getData(image1);
+      int[] data2 = getData(image2);
+      //data1 = grayscaleTransformation(data1);
+      //int[] data2 = grayLevelReductionOperatorByLevel(data1, 16);
+      int[] data = imageSubtraction(data1, data2);
+      //data = inverseOperator(data);
+      showImage(setData(image1, data));
    }
 
    private BufferedImage loadImage(String name) {
@@ -51,151 +38,18 @@ class Pixel {
          File file = new File(PATH + name);
          return ImageIO.read(file);
       } catch ( Exception e ) {
-         e.printStackTrace();
+           e.printStackTrace();
       }
       return null;
    }
 
-   private int[] process(int[] data, UnaryOperator<Pix> operator) {
-      return Arrays
-         .stream(data)
-         .parallel()
-         .map(
-            pixel -> {
-               return operator.apply(new Pix(pixel)).toInt();
-            }
-         )
-         .toArray();
-   }
-   
-   private int[] grayscaleTransformation(int[] data) {
-      return Arrays
-         .stream(data)
-         .parallel()
-         .map(
-            pixel -> {
-               return new Pix(pixel).toGrayInt();
-            }
-         )
-         .toArray();
-   }
-
-   private int[] inverseOperatorA(int[] data) {
-      UnaryOperator<Pix> operator = pix -> {
-         pix.red = 0xff - pix.red;
-         pix.green = 0xff - pix.green;
-         pix.blue = 0xff - pix.blue;
-         return pix;
-      };
-      return process(data, operator);
-   }
-
-   private int[] inverseOperatorB(int[] data) {
-       return Arrays
-         .stream(data)
-         .parallel()
-         .map(
-            pixel -> {
-               int a = (pixel >> 24) & 0xff;
-               int r = (pixel >> 16) & 0xff;
-               int g = (pixel >> 8) & 0xff;
-               int b = pixel & 0xff;
-
-               r = 0xff - r;
-               g = 0xff - g;
-               b = 0xff - b;
-               return (a << 24) | (r << 16) | (g << 8) | b;
-            }
-         )
-         .toArray();
-   }
-
-   private int[] binaryThresholdInterval(int[] data, int p1, int p2) {
-      return Arrays
-         .stream(data)
-         .parallel()
-         .map(
-            pixel -> {
-               int a = (pixel >> 24) & 0xff;
-               int r = (pixel >> 16) & 0xff;
-               int g = (pixel >> 8) & 0xff;
-               int b = pixel & 0xff;
-
-               r = (r >= p1 && r <= p2) ? 0xff : 0;
-               g = (g >= p1 && g <= p2) ? 0xff : 0;
-               b = (b >= p1 && b <= p2) ? 0xff : 0;
-               return (a << 24) | (r << 16) | (g << 8) | b;
-            }
-         )
-         .toArray();
-   }
-
-   private int[] invertedBinaryTresholdOperator(int[] data, int p1, int p2) {
-      return Arrays
-         .stream(data)
-         .parallel()
-         .map(
-            pixel -> {
-               int a = (pixel >> 24) & 0xff;
-               int r = (pixel >> 16) & 0xff;
-               int g = (pixel >> 8) & 0xff;
-               int b = pixel & 0xff;
-
-               r = (r >= p1 && r <= p2) ? 0 : 0xff;
-               g = (g >= p1 && g <= p2) ? 0 : 0xff;
-               b = (b >= p1 && b <= p2) ? 0 : 0xff;
-               return (a << 24) | (r << 16) | (g << 8) | b;
-            }
-         )
-         .toArray();
-   }
-
-   private int[] tresholdOperator(int[] data, int p1) {
-      return binaryThresholdInterval(data, p1, 0xff);
-   }
-
-   private int[] invertedTresholdOperator(int[] data, int p1) {
-      return binaryThresholdInterval(data, 0, p1);
-   }
-
-   private int[] grayscaleTresholdOperator(int[] data, int p1, int p2) {
-      return Arrays
-         .stream(data)
-         .parallel()
-         .map(
-            pixel -> {
-               int a = (pixel >> 24) & 0xff;
-               int r = (pixel >> 16) & 0xff;
-               int g = (pixel >> 8) & 0xff;
-               int b = pixel & 0xff;
-
-               r = (r >= p1 && r <= p2) ? r : 0;
-               g = (g >= p1 && g <= p2) ? g : 0;
-               b = (b >= p1 && b <= p2) ? b : 0;
-               return (a << 24) | (r << 16) | (g << 8) | b;
-            }
-         )
-         .toArray();
-   }
-
-   private int[] inverseGrayscaleTresholdOperator(int[] data, int p1, int p2) {
-      return Arrays
-         .stream(data)
-         .parallel()
-         .map(
-            pixel -> {
-               int a = (pixel >> 24) & 0xff;
-               int r = (pixel >> 16) & 0xff;
-               int g = (pixel >> 8) & 0xff;
-               int b = pixel & 0xff;
-
-               r = (r >= p1 && r <= p2) ? 0xff - r : 0;
-               g = (g >= p1 && g <= p2) ? 0xff - g : 0;
-               b = (b >= p1 && b <= p2) ? 0xff - b : 0;
-               return (a << 24) | (r << 16) | (g << 8) | b;
-            }
-         )
-         .toArray();
+   private void saveImage(BufferedImage image, String name, String format) {
+      try {
+         File file = new File(PATH + name);
+         ImageIO.write(image, format, file);
+      } catch ( Exception e ) {
+         e.printStackTrace();
+      }
    }
 
    private int[] getData(BufferedImage image) {
@@ -232,7 +86,254 @@ class Pixel {
             }
          }
       );
-    }
+   }
+
+   private int[] process(int[] data, UnaryOperator<Pix> operator) {
+      return Arrays
+         .stream(data)
+         .parallel()
+         .map(
+            pixel -> {
+               return operator.apply(new Pix(pixel)).toInt();
+            }
+         )
+         .toArray();
+   }
+
+   private int[] process(int[] data1, int[] data2, BinaryOperator<Pix> operator) {
+      List<Tuple<Integer, Integer>> data = new ArrayList<>();
+      for(int i = 0; i < data1.length && i < data2.length; i++) {
+         data.add(new Tuple<Integer, Integer>(data1[i], data2[i]));
+      }
+      return data
+         .parallelStream()
+         .mapToInt(
+            tuple -> {
+               return operator.apply(new Pix(tuple.first), new Pix(tuple.second)).toInt();
+            }
+         )
+         .toArray();
+   }
+   
+   private int[] grayscaleTransformation(int[] data) {
+      return Arrays
+         .stream(data)
+         .parallel()
+         .map(
+            pixel -> {
+               return new Pix(pixel).toGrayInt();
+            }
+         )
+         .toArray();
+   }
+
+   private int[] inverseOperator(int[] data) {
+      UnaryOperator<Pix> operator = pix -> {
+         pix.red = 0xff - pix.red;
+         pix.green = 0xff - pix.green;
+         pix.blue = 0xff - pix.blue;
+         return pix;
+      };
+      return process(data, operator);
+   }
+
+   private int[] binaryThresholdInterval(int[] data, int p1, int p2) {
+      UnaryOperator<Pix> operator = pix -> {
+         pix.red = (pix.red >= p1 && pix.red <= p2) ? 0xff : 0;
+         pix.green = (pix.green >= p1 && pix.green <= p2) ? 0xff : 0;
+         pix.blue = (pix.blue >= p1 && pix.blue <= p2) ? 0xff : 0;
+         return pix;
+      };
+      return process(data, operator);
+   }
+
+   private int[] invertedBinaryTresholdOperator(int[] data, int p1, int p2) {
+      UnaryOperator<Pix> operator = pix -> {
+         pix.red = (pix.red >= p1 && pix.red <= p2) ? 0 : 0xff;
+         pix.green = (pix.green >= p1 && pix.green <= p2) ? 0 : 0xff;
+         pix.blue = (pix.blue >= p1 && pix.blue <= p2) ? 0 : 0xff;
+         return pix;
+      };
+      return process(data, operator);
+   }
+
+   private int[] tresholdOperator(int[] data, int p1) {
+      return binaryThresholdInterval(data, p1, 0xff);
+   }
+
+   private int[] invertedTresholdOperator(int[] data, int p1) {
+      return binaryThresholdInterval(data, 0, p1);
+   }
+
+   private int[] grayscaleTresholdOperator(int[] data, int p1, int p2) {
+      UnaryOperator<Pix> operator = pix -> {
+         pix.red = (pix.red >= p1 && pix.red <= p2) ? pix.red : 0;
+         pix.green = (pix.green >= p1 && pix.green <= p2) ? pix.green : 0;
+         pix.blue = (pix.blue >= p1 && pix.blue <= p2) ? pix.blue : 0;
+         return pix;
+      };
+      return process(data, operator);
+   }
+
+   private int[] inverseGrayscaleTresholdOperator(int[] data, int p1, int p2) {
+      UnaryOperator<Pix> operator = pix -> {
+         pix.red = (pix.red >= p1 && pix.red <= p2) ? 0xff - pix.red : 0;
+         pix.green = (pix.green >= p1 && pix.green <= p2) ? 0xff - pix.green : 0;
+         pix.blue = (pix.blue >= p1 && pix.blue <= p2) ? 0xff - pix.blue : 0;
+         return pix;
+      };
+      return process(data, operator);
+   }
+
+
+   private int[] stretchOperator(int[] data, int p1, int p2) {
+      UnaryOperator<Pix> operator = pix -> {
+         //((x - p1)*(255 / (p2 - p1))) if x > p1 and x < p2 else 0
+
+         pix.red = (pix.red > p1 && pix.red < p2) ? (pix.red - p1) * (0xff / (p2 - p1)) : 0;
+         pix.green = (pix.green > p1 && pix.green < p2) ? (pix.green - p1) * (0xff / (p2 - p1)) : 0;
+         pix.blue = (pix.blue > p1 && pix.blue < p2) ? (pix.blue - p1) * (0xff / (p2 - p1)) : 0;
+         return pix;
+      };
+      return process(data, operator);
+   }
+
+   private int[] grayLevelReductionOperator(int[] data, int[] p, int[] q) {
+      UnaryOperator<Pix> operator = pix -> {
+         boolean[] over = new boolean[3];
+         over[0] = over[1] = over[2] = true;
+
+         for (int i = 0; i < p.length && (over[0] || over[1] || over[2]); i++) {
+            if (over[0] && pix.red <= p[i]) {
+               over[0] = false;
+               pix.red = i == 0 ? 0 : q[i - 1];
+            }
+            if (over[1] && pix.green <= p[i]) {
+               over[1] = false;
+               pix.green = i == 0 ? 0 : q[i - 1];
+            }
+            if (over[2] && pix.blue <= p[i]) {
+               over[2] = false;
+               pix.blue = i == 0 ? 0 : q[i - 1];
+            }
+         }
+         if (over[0]) {
+            pix.red = 0xff;
+         }
+         if (over[1]) {
+            pix.green = 0xff;
+         }
+         if (over[2]) {
+            pix.blue = 0xff;
+         }
+         return pix;
+      };
+      return process(data, operator);
+   }
+
+   private int[] grayLevelReductionOperatorByLevel(int[] data, int levels) {
+      if(levels < 2)
+         return data;
+
+      int[] p = new int[levels];
+      for(int i = 0; i < levels; i++) {
+         p[i] = 0xff / levels * i;
+      }
+      levels--;
+      int[] q = new int[levels];
+      for(int i = 0; i < levels; i++) {
+         q[i] = 0xff / levels * i;
+      }
+      return grayLevelReductionOperator(data, p , q);
+   }
+
+   private int[] imageAddition(int[] data1, int[] data2) {
+      return imageAddition(data1, data2, 2);
+     }
+
+   private int[] imageAddition(int[] data1, int[] data2, double scalingFactor) {
+      BinaryOperator<Pix> operator = (pix1, pix2) -> {
+         Pix pix = new Pix();
+         pix.red = (int) Math.ceil( (pix1.red + pix2.red) / scalingFactor);
+         pix.green = (int) Math.ceil( (pix1.green + pix2.green) / scalingFactor);
+         pix.blue = (int) Math.ceil( (pix1.blue + pix2.blue) / scalingFactor);
+         return pix;
+      };
+
+      return process(data1, data2, operator);
+   }
+
+   private int[] imageSubtraction(int[] data1, int[] data2) {
+      List<Tuple<Integer, Integer>> data = new ArrayList<>();
+      for(int i = 0; i < data1.length && i < data2.length; i++)
+         data.add(new Tuple<Integer, Integer>(data1[i], data2[i]));
+      
+      List<Pix> subtractedData = data
+         .parallelStream()
+         .map(
+            tuple -> {
+               Pix pix1 = new Pix(tuple.first);
+               Pix pix2 = new Pix(tuple.second);
+               Pix pix = new Pix();
+               pix.red = pix1.red - pix2.red;
+               pix.green = pix1.green - pix2.green;
+               pix.blue = pix1.blue - pix2.blue;   
+               return pix;
+            }
+         )
+         .collect(Collectors.toList());
+
+      int[] max = new int[3];
+      int[] min = new int[3];
+
+      max[0] = subtractedData
+         .stream()
+         .mapToInt( pix -> pix.red)
+         .max()
+         .getAsInt();
+      max[1] = subtractedData
+         .stream()
+         .mapToInt( pix -> pix.green)
+         .max()
+         .getAsInt();
+      max[2] = subtractedData
+         .stream()
+         .mapToInt( pix -> pix.blue)
+         .max()
+         .getAsInt();
+      min[0] = subtractedData
+         .stream()
+         .mapToInt( pix -> pix.red)
+         .min()
+         .getAsInt();
+      min[1] = subtractedData
+         .stream()
+         .mapToInt( pix -> pix.green)
+         .min()
+         .getAsInt();
+      min[2] = subtractedData
+         .stream()
+         .mapToInt( pix -> pix.blue)
+         .min()
+         .getAsInt();
+
+      double[] scalar = new double[3];
+      scalar[0] = (255.0 / (Math.abs(min[0]) + Math.abs(max[0])));
+      scalar[1] = (255.0 / (Math.abs(min[1]) + Math.abs(max[1])));
+      scalar[2] = (255.0 / (Math.abs(min[2]) + Math.abs(max[2])));
+
+      return subtractedData
+         .parallelStream()
+         .mapToInt( pix ->
+            {
+               pix.red = Math.toIntExact(Math.round((pix.red - min[0]) * scalar[0]));
+               pix.green = Math.toIntExact(Math.round((pix.green - min[1]) * scalar[1]));
+               pix.blue = Math.toIntExact(Math.round((pix.blue - min[2]) * scalar[2]));
+               return pix.toInt();
+            }
+         )
+         .toArray();
+   }
 
    static public void main(String args[]) throws Exception {
       Pixel obj = new Pixel();
@@ -245,6 +346,8 @@ class Pix {
    int red;
    int green;
    int blue;
+
+   Pix() {}
 
    Pix(int pixel) {
       alpha = (pixel >> 24) & 0xff;
@@ -263,7 +366,17 @@ class Pix {
                      0.21 * red + 0.72 * green + 0.07 * blue
                   )
                );
-      return (alpha << 24) | (red << 16) | (green << 8) | blue;  
+      return (alpha << 24) | (avg << 16) | (avg << 8) | avg;  
    }
 
+}
+
+class Tuple<E, F> {
+   final E first;
+   final F second;
+
+   Tuple(E e, F f) {
+      first = e;
+      second = f;
+   }
 }
